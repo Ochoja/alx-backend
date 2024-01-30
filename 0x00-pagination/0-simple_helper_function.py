f05514039ac9d029e76fcc431ba72b69905c8ee8
Daniel Ochoja <danielochoja@yahoo.com>
2024-01-30 14:22:22 +0100

    renamed file

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env/ python3
 """Simple helper function for pagination"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns start and end index"""
     return ((page_size * page) - page_size, page_size * page)
